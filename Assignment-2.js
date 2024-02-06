5bb409556b25d785501d63869bec966fd3d07b69
unknown <smbhavanishesha@gmail.com>
2024-02-06 11:07:46 +0530

    Assignments 2

---
+++ Assignment-2.js
@@ -0,0 +1,14 @@
+/********let x = [1, 3454, 655, 24, 7, 8] find the largest number in x without using sort method*******/
+
+let x =[1, 3454, 655, 24, 7, 8]
+let largest =0
+
+/** using for loop **/
+for(i=0;i<x.length;i++)
+{
+	if(x[i]>largest)
+    {
+     largest=x[i]
+	}
+}
+console.log(largest)
